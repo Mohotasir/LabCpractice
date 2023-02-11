110b303be476211e81326d82ef256fca3fbda579
mohotasir <talukdertahsin643@gmail.com>
2023-02-11 21:27:58 +0600

    new file:   heart.c

---
+++ heart.c
@@ -0,0 +1,47 @@
+
+#include<stdio.h>
+int main(){
+      int i,j,k,n,s;
+     scanf("%d",&n);
+    //upside
+     for(int u =2;u<n/2;u++){
+          for(int s = n/2 ; s >= u ; s--){
+            printf(" ");
+         }
+
+         for(int v = 1;v<=u;v++){
+
+            printf("* ");
+
+         }
+         for(int s = n/2 ; s >= u ; s--){
+            printf(" ");
+         }
+
+         for(int v = 1;v<=u;v++){
+
+            printf("* ");
+
+         }
+         printf("\n");
+     }
+
+
+    //downside
+    for(i=1;i<=n;i++){
+            for(k=1;k<=i;k++){
+            printf(" ");
+        }
+        for(j=n ;j>i;j--){
+            printf("* ");
+        }
+
+
+        printf("\n");
+    }
+
+
+
+    return 0;
+}
+
